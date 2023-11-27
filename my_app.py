db1928cb7b35720d2a6175f3210cdb29c18c75e7
Mariam94Alghamdi <mariam.alghamdi94@gmaul.com>
2023-11-27 22:20:21 +0300

    updates

--- my_app.py
+++ my_app.py
@@ -3,7 +3,6 @@ import pickle
 import pandas as pd
 from sklearn.compose import make_column_transformer
 from sklearn.preprocessing import OrdinalEncoder
-from sklearn import *
 
 st.sidebar.title('Car Price Prediction')
 html_temp = """
@@ -12,31 +11,26 @@ html_temp = """
 </div>"""
 st.markdown(html_temp, unsafe_allow_html=True)
 
-car_model=st.sidebar.selectbox("Select model of your car", ('Audi A1', 'Audi A3', 'Opel Astra', 'Opel Corsa', 'Opel Insignia', 'Renault Clio', 'Renault Duster', 'Renault Espace'))
-vat=st.sidebar.radio('Select vat type',('VAT deductible','Price negotiable'))
-km=st.sidebar.slider("What is the km of your car", 0,317000, step=1000)
-Type=st.sidebar.radio('Select Used type',('Used',"Employee's car",'New', 'Demonstration', 'Pre-registered'))
-Fuel=st.sidebar.radio('Select Fuel type',('Diesel','Benzine','LPG/CNG', 'Electric'))
-age=st.sidebar.selectbox("What is the age of your car:",(0,1,2,3))
-Upholstery_type=st.sidebar.radio('Select Upholstery_type type',('Cloth','Part/Full Leather'))
-gearing_type=st.sidebar.radio('Select gear type',('Automatic','Manual','Semi-automatic'))
 
-ds13_model=pickle.load(open("final_model_new","rb"))
+age=st.sidebar.selectbox("What is the age of your car:",(0,1,2,3))
+hp=st.sidebar.slider("What is the hp_kw of your car?", 40, 300, step=5)
+km=st.sidebar.slider("What is the km of your car", 0,350000, step=1000)
+gearing_type=st.sidebar.radio('Select gear type',('Automatic','Manual','Semi-automatic'))
+car_model=st.sidebar.selectbox("Select model of your car", ('Audi A1', 'Audi A3', 'Opel Astra', 'Opel Corsa', 'Opel Insignia', 'Renault Clio', 'Renault Duster', 'Renault Espace'))
+
+
+ds13_model=pickle.load(open("rf_model_new","rb"))
 ds13_transformer = pickle.load(open('transformer', 'rb'))
 
 
-
 my_dict = {
-    "make_model": car_model,
-    "vat": vat,
-    "km": km,
-    "Type": Type,
-    "Fuel": Fuel,
     "age": age,
-    "Upholstery_type": Upholstery_type,
-    "Gearing_Type": gearing_type
+    "hp_kW": hp,
+    "km": km,
+    'Gearing_Type':gearing_type,
+    "make_model": car_model
+    
 }
-
 
 df = pd.DataFrame.from_dict([my_dict])
 
